1fa1c6ede4d3387913d3b64195cc3a7ed36ceb47
jmildner <johann.mildner@balcab.ch>
2021-03-16 16:32:24 +0100

    Initial commit

---
+++ src/main/java/ch/jmildner/App.java
@@ -0,0 +1,9 @@
+package ch.jmildner;
+
+import ch.jmildner.tools11.MyTools;
+
+public class App {
+    public static void main( String[] args )    {
+        MyTools.h2(MyTools.getVersion());
+    }
+}
