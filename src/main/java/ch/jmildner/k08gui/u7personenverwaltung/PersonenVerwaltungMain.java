61ea78c8eed2cca8e556d666cd066ecad81f3401
jmildner <johann.mildner@balcab.ch>
2021-03-19 12:18:06 +0100

    jmb

---
+++ src/main/java/ch/jmildner/k08gui/u7personenverwaltung/PersonenVerwaltungMain.java
@@ -0,0 +1,8 @@
+
+package ch.jmildner.k08gui.u7personenverwaltung;
+
+public class PersonenVerwaltungMain {
+    public static void main(String[] args) {
+        new PersonenVerwaltungFrame(true);
+    }
+}
